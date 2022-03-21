b1e1a7070956453042fa13eeb1d5ba0f96d53450
Jovanoskaelena <101874636+Jovanoskaelena@users.noreply.github.com>
2022-03-21 14:30:22 +0100

    Add project files.

---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne, numberTwo, third;
+            Console.WriteLine("Enter the first Number:");
+            numberOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Number:");
+            numberTwo = int.Parse(Console.ReadLine());
+            third = numberOne;
+            numberOne = numberTwo;
+            numberTwo = third;
+            Console.WriteLine("After Swapping : ");
+            Console.WriteLine("First Number : " + numberOne);
+            Console.WriteLine("Second Number : " + numberTwo);
+            Console.Read();
+        }
+    }
+}
